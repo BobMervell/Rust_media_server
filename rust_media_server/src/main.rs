8f5c8f0034137dcb11dc9e1cd2ac08dbd84d7f14
Bob Mervell <bob_mervell.mailbox@proton.me>
2025-12-15 20:32:40 +0100

    Feat: Testing exploring smb share

--- rust_media_server/src/main.rs
+++ rust_media_server/src/main.rs
@@ -1,3 +1,34 @@
-fn main() {
-    println!("Hello, world!");
+use smb::{Client, ClientConfig, FileAccessMask, FileAllInformation, Resource, UncPath};
+use std::str::FromStr;
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+     let client = Client::new(ClientConfig::default());
+    println!("bonsoir");
+    // Connect to a share
+    let target_path: UncPath = UncPath::from_str(r"\\server\path").unwrap();
+    client.share_connect(&target_path, "name", "le mot de passe".to_string()).await?;
+    
+
+    let tree = client.get_tree(&target_path).await?;
+    let access_mask = FileAccessMask::new().with_generic_read(true);
+
+    let resource = tree.open_existing("anora (2024)",access_mask).await?;
+
+    match &resource {
+        Resource::File(_file) => {
+            println!("We have a file");
+        }
+        Resource::Directory(dir) => {
+            println!("We have a dir");
+            let output = dir.query_info::<FileAllInformation>().await?;
+            let truc= &output.name.file_name;
+            println!("{}", truc);
+        }
+        Resource::Pipe(_pipe) => {
+            println!("We have a pipe");
+        }
+    }
+ 
+    Ok(())
 }
