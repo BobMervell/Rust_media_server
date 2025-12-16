e2cf07c7074fbd7baaa535871c766ceaac6c51d3
Bob Mervell <bob_mervell.mailbox@proton.me>
2025-12-16 19:10:46 +0100

    Feat: draft algo for exploring a smb share

--- rust_media_server/src/main.rs
+++ rust_media_server/src/main.rs
@@ -1,5 +1,6 @@
-use smb::{Client, ClientConfig, FileAccessMask, FileAllInformation, Resource, UncPath};
-use std::str::FromStr;
+use smb::{Client, ClientConfig, Directory, FileAccessMask, FileAllInformation, FileDirectoryInformation, Resource, UncPath};
+use std::{str::FromStr, sync::Arc};
+use trpl::StreamExt;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
@@ -7,23 +8,37 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     println!("bonsoir");
     // Connect to a share
     let target_path: UncPath = UncPath::from_str(r"\\server\path").unwrap();
-    client.share_connect(&target_path, "name", "le mot de passe".to_string()).await?;
+    client.share_connect(&target_path, "username", "mdp".to_string()).await?;
     
 
     let tree = client.get_tree(&target_path).await?;
     let access_mask = FileAccessMask::new().with_generic_read(true);
 
-    let resource = tree.open_existing("anora (2024)",access_mask).await?;
+    let resource = tree.open_existing("",access_mask).await?;
 
-    match &resource {
+
+    match resource {
         Resource::File(_file) => {
             println!("We have a file");
         }
         Resource::Directory(dir) => {
             println!("We have a dir");
-            let output = dir.query_info::<FileAllInformation>().await?;
-            let truc= &output.name.file_name;
-            println!("{}", truc);
+            // let output = dir.query_info::<FileAllInformation>().await?;
+            // let truc= &output.name.file_name;
+            let dir: Arc<Directory> = Arc::new(dir);
+            let mut test_stream: smb::resource::iter_stream::QueryDirectoryStream<'_, FileDirectoryInformation> =  smb::Directory::query::<FileDirectoryInformation>(&dir,"*").await?;
+             // Iterate through the entries in the directory using next()
+            while let Some(entry) = test_stream.next().await {
+                match entry {
+                    Ok(file_info) => {
+                        println!("Found file: {:?}", file_info.file_name);
+                    }
+                    Err(e) => {
+                        eprintln!("Error retrieving entry: {:?}", e);
+                        break; // Exit on error
+                    }
+                }
+            }
         }
         Resource::Pipe(_pipe) => {
             println!("We have a pipe");
