3dac5ed3d6f2d26c0c19f928fa28883a174511c0
Bob Mervell <bob_mervell.mailbox@proton.me>
2025-12-17 21:32:31 +0100

    Feat: Maintenant on arrive a ouvrir les sub directory

--- rust_media_server/src/main.rs
+++ rust_media_server/src/main.rs
@@ -1,12 +1,31 @@
-use smb::{Client, ClientConfig, Directory, FileAccessMask, FileDirectoryInformation, Resource, UncPath};
+use smb::{Client, ClientConfig, Directory, FileAccessMask, FileAttributes, FileDirectoryInformation, Resource, UncPath};
 use std::{io, str::FromStr, sync::Arc};
 use trpl::StreamExt;
+
+
+struct ClientInfo {
+    client: smb::Client,
+    path: UncPath,
+}
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
 
-    let mut path = String::new();
+    
 
+    let client_info = smb_connect().await?;
+    let tree: Arc<smb::Tree> = client_info.client.get_tree(&client_info.path).await?;
+
+    let path = client_info.path.to_string();
+    explore_smb_dir(tree,&path).await?;
+
+    Ok(())
+}
+
+
+async fn smb_connect() ->  Result<ClientInfo, Box<dyn std::error::Error>> {
+
+    let mut path = String::new();
     println!("Enter the samba remote path");
     io::stdin().read_line(&mut path)?;
     let path = path.trim_end(); 
@@ -21,54 +40,66 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     io::stdin().read_line(&mut passwd)?;
     let passwd=passwd.trim_end();
 
-    let resource = smb_connect(path, username, passwd).await?;
-
-    match resource {
-        Resource::File(_file) => {
-            println!("Unvalid  path: {}. This path leads to a file, should be a directory. ", path);
-        }
-        Resource::Directory(dir) => {
-            println!("We have a dir");
-            explore_smb_dir(dir).await?;
-           
-        }
-        Resource::Pipe(_pipe) => {
-            println!("Unvalid  path: {}. This path leads to a pipe, should be a directory. ", path);
-        }
-    }
-
-    Ok(())
+    let client = Client::new(ClientConfig::default());
+    let path: UncPath = UncPath::from_str(path).unwrap();
+    client.share_connect(&path, username, passwd.to_string()).await?;
+    Ok(ClientInfo {client,path})
+    
 }
 
 
-async fn smb_connect(path:&str,username:&str,passwd:&str) ->  Result<Resource, Box<dyn std::error::Error>> {
-    let client = Client::new(ClientConfig::default());
-    let target_path: UncPath = UncPath::from_str(path).unwrap();
-    client.share_connect(&target_path, username, passwd.to_string()).await?;
-    
-
-    let tree = client.get_tree(&target_path).await?;
+async fn explore_smb_dir (tree:Arc<smb::Tree>, root_path:&str) ->  Result<(), Box<dyn std::error::Error>> {
+    let root_path = "";
     let access_mask = FileAccessMask::new().with_generic_read(true);
+    println!("no");
 
     let resource = tree.open_existing("",access_mask).await?;
-    return Ok(resource);
-}
+    println!("yes");
+    match resource {
+        Resource::File(_file) => {
+            Err(format!("The path « {} » is not a directory but a file", root_path).into())
+        }
+        Resource::Pipe(_pipe) => {
+            Err(format!("The path « {} » is not a directory but a pipe", root_path).into())
+        }
+        Resource::Directory(dir) => {
+            let dir: Arc<Directory> = Arc::from(dir);
+            let mut data_stream=  smb::Directory::query::<FileDirectoryInformation>(&dir,"*").await?;
+            while let Some(entry) = data_stream.next().await {
+                match entry {
+                    Ok(file_info) => {
+                        if file_info.file_attributes.directory() {
+                            let sub_path = file_info.file_name.to_string();
+                             if sub_path == "." || sub_path == ".." {
+                                continue;
+                            }
+                            println!("{}", sub_path);
+                            let resource = tree.open_existing(&sub_path,access_mask).await?;
+                            match resource {
+                                Resource::File(_file) => {
+                                    println!("Unvalid  path: {}. This path leads to a file, should be a directory. ", sub_path);
+                                }
+                                Resource::Directory(dir) => {
+                                    println!("We have a dir {}",sub_path);     
+                                }
+                                Resource::Pipe(_pipe) => {
+                                    println!("Unvalid  path: {}. This path leads to a pipe, should be a directory. ", sub_path);
+                                }
+                            }
 
-
-async fn explore_smb_dir (root_dir:Directory) ->  Result<(), Box<dyn std::error::Error>> {
-    let root_dir: Arc<Directory> = Arc::new(root_dir);
-    let mut test_stream=  smb::Directory::query::<FileDirectoryInformation>(&root_dir,"*").await?;
-        // Iterate through the entries in the directory using next()
-    while let Some(entry) = test_stream.next().await {
-        match entry {
-            Ok(file_info) => {
-                println!("Found file: {:?}", file_info.file_name);
+                        }
+                        else if !file_info.file_attributes.directory() {
+                            println!("{}", file_info.file_name)
+                        }
+                    }
+                    Err(e) => {
+                        eprintln!("Error retrieving entry: {:?}", e);
+                        break; // Exit on error
+                    }
+                }
             }
-            Err(e) => {
-                eprintln!("Error retrieving entry: {:?}", e);
-                break; // Exit on error
-            }
+            Ok(())
         }
     }
-     Ok(())
 }
+
