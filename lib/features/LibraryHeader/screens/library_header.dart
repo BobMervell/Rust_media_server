cc478c17730c48fd5cf02fda2d167664fc5c961b
Bob Mervell <bob_mervell.mailbox@proton.me>
2026-03-04 15:52:00 +0100

    Feat: Added library header

---
+++ lib/features/LibraryHeader/screens/library_header.dart
@@ -0,0 +1,20 @@
+import 'package:fluster_media_center/features/LibraryHeader/widgets/placeholder_banner.dart';
+import 'package:flutter/material.dart';
+
+class LibraryHeader extends StatelessWidget {
+  const LibraryHeader({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SliverToBoxAdapter(
+      child: Container(
+        height: 500,
+        decoration: BoxDecoration(
+          color: Color.fromARGB(255, 200, 220, 220),
+          borderRadius: BorderRadius.all(Radius.circular(20)),
+        ),
+        child: PlaceholderBanner(),
+      ),
+    );
+  }
+}
---
+++ lib/features/LibraryHeader/widgets/placeholder_banner.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class PlaceholderBanner extends StatelessWidget {
+  const PlaceholderBanner({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(child: Text("Have a nice time here"));
+  }
+}
