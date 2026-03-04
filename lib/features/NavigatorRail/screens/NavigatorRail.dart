54dd907e699e2e146415a2d3eeb66e7a9175beb1
Bob Mervell <bob_mervell.mailbox@proton.me>
2026-03-04 14:38:47 +0100

    Feat: Added an adaptative NavigatorRail/dock ( placeholder buttons only)

---
+++ lib/features/NavigatorRail/screens/NavigatorRail.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:fluster_media_center/features/NavigatorRail/widgets/NavigatorDock.dart';
+
+class NavigatorRail extends StatelessWidget {
+  const NavigatorRail({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return LayoutBuilder(
+      builder: (context, constraints) {
+        final maxHeight = constraints.maxHeight;
+
+        final targetHeight = maxHeight / 2;
+
+        //TODO tune or replace magic number for dock height
+        final useFullHeight = maxHeight < 600;
+
+        return Column(
+          children: [
+            if (!useFullHeight) const Spacer(),
+
+            SizedBox(
+              height: useFullHeight ? maxHeight : targetHeight,
+              child: NavigatorDock(),
+            ),
+
+            if (!useFullHeight) const Spacer(),
+          ],
+        );
+      },
+    );
+  }
+}
